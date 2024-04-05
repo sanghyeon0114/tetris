1f9f6c7ea96b680f4f2af3856fd9112b15a062a9
황상현 <sanghyeon@hwangsanghyeon-ui-MacBookPro.local>
2024-04-06 05:23:36 +0900

    게임 시작 화면 추가

--- game.cpp
+++ game.cpp
@@ -1,1 +1,76 @@
 #include "game.h"
+#include "console/console.h"
+
+Game::Game() {
+    // Todo: board 처리
+    tick = 0;
+}
+
+void Game::inputKey() {
+    if(console::key(console::K_NONE)) {
+
+    } else if(console::key(console::K_OTHER)) {
+
+    } else if(console::key(console::K_LEFT)) {
+
+    } else if(console::key(console::K_RIGHT)) {
+
+    } else if(console::key(console::K_UP)) {
+
+    } else if(console::key(console::K_DOWN)) {
+
+    } else if(console::key(console::K_ESC)) {
+        exit(0);
+    } else if(console::key(console::K_SPACE)) {
+
+    } else if(console::key(console::K_Z)) {
+
+    } else if(console::key(console::K_X)) {
+
+    } else if(console::key(console::K_ENTER)) {
+        
+    }
+}
+
+// 게임의 한 프레임을 처리한다.
+void Game::update() {
+    // input Code.
+    Game::inputKey();
+    if(++tick == DROP_DELAY) {
+        // input Code 2.
+        tick = 0;
+    }
+}
+
+void Game::drawGameBox() {
+    // can use [1 ~ BOARD_WIDTH][1 ~ BOARD_HEIGHT]
+    console::drawBox(0, 0, BOARD_WIDTH+1, BOARD_HEIGHT+1);
+}
+
+void Game::drawNextBox() {
+    int startPosX = (BOARD_WIDTH+1)+2;
+    int startPosY = 0;
+    int boxSize = 5;
+    console::drawBox(startPosX, startPosY, startPosX+boxSize, startPosY+boxSize);
+    console::draw(startPosX+1, startPosY, "Next");
+}
+
+void Game::drawHoldBox() {
+    int startPosX = ((BOARD_WIDTH+1)+2+5)+1;
+    int startPosY = 0;
+    int boxSize = 5;
+    console::drawBox(startPosX, startPosY, startPosX+boxSize, startPosY+boxSize);
+    console::draw(startPosX+1, startPosY, "Hold");
+}
+
+// 게임 화면을 그린다.
+void Game::draw() {
+    Game::drawGameBox();
+    Game::drawNextBox();
+    Game::drawHoldBox();
+}
+
+// 게임 루프가 종료되어야 하는지 여부를 반환한다.
+bool Game::shouldExit() {
+    return false;
+}
--- tetromino.cpp
+++ tetromino.cpp
@@ -1,1 +1,42 @@
 #include "tetromino.h"
+#include <string>
+
+Tetromino Tetromino::I("I", 4, "XXXXOOOOXXXXXXXX");
+Tetromino Tetromino::O("O", 2, "OOOO");
+Tetromino Tetromino::T("T", 3, "XOXOOOXXX");
+Tetromino Tetromino::S("S", 3, "XOOOOXXXX");
+Tetromino Tetromino::Z("Z", 3, "OOXXOOXXX");
+Tetromino Tetromino::J("J", 3, "OXXOOOXXX");
+Tetromino Tetromino::L("L", 3, "XXOOOOXXX");
+
+Tetromino::Tetromino(std::string name, int size, std::string shape) {
+    name_ = name;
+    size_ = size;
+    original_ = this;
+
+    for(int i = 0; i < size_; i++) {
+        for(int j = 0; j < size_; j++) {
+            if(shape[i*4+j] == 'O') {
+                shape_[i][j] = true;
+            } else {
+                shape_[i][j] = false;
+            }
+        }
+    }
+}
+
+// 시계 방향으로 회전한 모습의 테트로미노 객체를 반환한다.
+Tetromino Tetromino::rotatedCW() {
+    //Tetromino result = Tetromino(this->name_, this->size_, "");
+    return I;
+}
+
+// 반시계 방향으로 회전한 모습의 테트로미노 객체를 반환한다.
+Tetromino Tetromino::rotatedCCW() {
+    return I;
+}
+
+// 화면의 x, y 위치에 s 문자열로  테트로미노를 그린다
+void Tetromino::drawAt(std::string s, int x, int y) {
+
+}
